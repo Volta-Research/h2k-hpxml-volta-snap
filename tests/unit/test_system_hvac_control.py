bcc82270755190598b45bf0417a964a6c9a00e3c
Aidan Brookson <aidanbrookson@voltaresearch.org>
2026-06-26 15:52:18 -0400

    Updated HVAC control unit test to align with new set points

--- tests/unit/test_system_hvac_control.py
+++ tests/unit/test_system_hvac_control.py
@@ -5,6 +5,7 @@ from unittest.mock import patch
 
 from h2k_hpxml.components.system_hvac_control import get_hvac_control
 from h2k_hpxml.core.model import ModelData
+from h2k_hpxml.utils.operating_conditions import get_soc_house_parameters
 
 H2K_SETPOINTS = {
     "setpoint_heating_day": 68.0,
@@ -20,6 +21,7 @@ def _mock_h2k_numbers(_h2k_dict, field_key):
 
 def _make_model_data(apply_temperature_setback=None):
     model_data = ModelData()
+    model_data.set_operating_conditions("SOC", get_soc_house_parameters())
     config_manager = MagicMock()
     if apply_temperature_setback is None:
         config_manager.get_bool.return_value = False
@@ -67,6 +69,7 @@ def test_apply_temperature_setback_true_preserves_h2k_setback(_mock_get_number):
 )
 def test_missing_config_manager_defaults_to_no_setback(_mock_get_number):
     model_data = ModelData()
+    model_data.set_operating_conditions("SOC", get_soc_house_parameters())
 
     result = get_hvac_control({}, model_data)
 
